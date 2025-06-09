ca0454765f66ff02f75a28e070ae3cea1362b030
trinhtan <trinhtan1703@gmail.com>
2025-06-09 12:55:51 +0700

    a

--- database/database.py
+++ database/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker
 from models.models_giao_dich import Base
 
 DATABASE_URL = "postgresql://quanlychitieu_user:MfrzOGP5r1rpO1cRQrsQCw58rIb34fka@dpg-d115toumcj7s739smp30-a.singapore-postgres.render.com/quanlychitieu"
-
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
