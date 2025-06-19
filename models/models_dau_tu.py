6b19835b25ebc2f47b1b66c4d76c12e0776766d0
trinhtan <trinhtan1703@gmail.com>
2025-06-19 12:45:08 +0700

    sua id dautui

--- models/models_dau_tu.py
+++ models/models_dau_tu.py
@@ -5,7 +5,7 @@ from models.base import Base
 class DauTu(Base):
     __tablename__ = "dau_tu"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     ghi_chu = Column(String)
     ngay_nhap = Column(TIMESTAMP)
     tong_so_tien = Column(Integer)
